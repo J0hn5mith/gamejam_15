a8a438cba8de6b4aaca379ca7fd2fcab97162e6b
ZYXer- <github@henryraymond.com>
2015-01-25 15:45:06 +0100

    Removed unused audio and img

--- js/resources.js
+++ js/resources.js
@@ -2,14 +2,10 @@ function Resources() {
 
   
   this.images = {
-    test : "img/test.png",
-    test2 : "img/test2.png",
-    test3 : "img/test3.jpg"
   };
 
   
   this.sounds = {
-    cannon : { source : ["audio/cannon.wav", "audio/cannon.mp3"], instances : 3 },
   };
   
 }
